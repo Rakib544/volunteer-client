de4f695aabefe9e681bb58512a19badfbd5e7a30
Rakib544 <md.rakib10122003@gmail.com>
2021-03-29 19:40:15 +0600

    login validation done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import { Button, Grid, makeStyles, Paper, TextField, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const useStyles = makeStyles((theme) => ({
@@ -16,12 +16,22 @@ const useStyles = makeStyles((theme) => ({
     formStyle: {
         display: 'flex',
         justifyContent: 'center'
+    },
+    span: {
+        color: 'tomato',
+        cursor: 'pointer',
+    },
+    googleButton: {
+        display: 'block',
+        margin: '10px auto'
     }
 }))
 
 const Login = () => {
     const classes = useStyles()
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, errors } = useForm();
+
+    const [showLogin, setShowLogin] = useState(true)
 
     const onSubmit = data => {
         console.log(data)
@@ -30,14 +40,68 @@ const Login = () => {
         <div className={classes.formStyle}>
             <Grid lg="5" justify="center" className={classes.grid}>
                 <Paper className={classes.paper}>
-                    <Typography variant="h6">Login Here</Typography>
-                    <form onSubmit = {handleSubmit(onSubmit)}>
-                        <TextField name="name" label="Full Name" className={classes.input} inputRef={register} />
-                        <TextField name="email" label="Enter Your Email" className={classes.input} inputRef={register} />
-                        <TextField name="password" label="Enter Your password" className={classes.input} inputRef={register} />
-                        <TextField name="description" label="Description" className={classes.input} inputRef={register} />
-                        <Button type="submit" variant="contained" color="primary">Login</Button>
+                    <Typography variant="h6">
+                        {showLogin ? 'Login Here' : 'Registration'}
+                    </Typography>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        {!showLogin &&
+                            <TextField
+                                name="name"
+                                label="Full Name"
+                                className={classes.input}
+                                inputRef={register({
+                                    required: 'Name is required'
+                                })}
+                                error={Boolean(errors.name)}
+                                helperText={errors.name?.message}
+                            />
+                        }
+                        <TextField
+                            name="email"
+                            label="Enter Your Email"
+                            className={classes.input}
+                            inputRef={register({
+                                required: 'email is required'
+                            })}
+                            error={Boolean(errors.email)}
+                            helperText={errors.email?.message}
+                        />
+                        <TextField
+                            name="password"
+                            label="Enter Your password"
+                            className={classes.input}
+                            inputRef={register({
+                                required: 'password is required'
+                            })} 
+                            error = {Boolean(errors.password)}
+                            helperText={errors.password?.message}
+                            type="password"
+                        />
+                        {!showLogin &&
+                            <TextField
+                                name="description"
+                                label="Description"
+                                className={classes.input}
+                                inputRef={register({
+                                    required: 'Description is required'
+                                })}
+                                error={Boolean(errors.description)}
+                                helperText={errors.description?.message}
+                            />
+                        }
+                        <Button type="submit" variant="contained" color="primary">
+                            {showLogin ? ' Login' : 'Registration'}
+                        </Button>
                     </form>
+                    <Button variant="contained" color="primary" className={classes.googleButton}>
+                        Continue WIth Google
+                        </Button>
+                    <Typography variant="subtitle1" align="center">
+                        {showLogin ? "Don't Have any account ?" : "Already Have an account ? "}
+                        <strong className={classes.span} onClick={() => setShowLogin(!showLogin)}>
+                            {showLogin ? 'Registration Here' : 'Login Here'}
+                        </strong>
+                    </Typography>
                 </Paper>
             </Grid>
         </div>
